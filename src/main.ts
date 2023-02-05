c68b88dcdda9c5f96057a2f09b5cab2dbe5eee91
2hoyeong <leehodud302@naver.com>
2023-02-05 15:57:18 +0900

    feat: app port env로 설정되도록 구현

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
 
   app.setGlobalPrefix(configService.get('app.prefix'));
 
-  await app.listen(3000);
+  await app.listen(configService.get('app.port'));
 }
 bootstrap();
